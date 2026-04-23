d887809e33a428addd7dafdbfef8a30c6ec8c85e
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-04-23 11:21:48 +0300

    Solution 3.0

--- app/main.py
+++ app/main.py
@@ -6,22 +6,25 @@ import requests
 def get_weather() -> None:
     URL = "https://api.weatherapi.com/v1/current.json"
     CITY = "Paris"
-    API_KEY = os.environ["API_KEY"]
+    API_KEY = os.environ.get("API_KEY")
     response = requests.get(
         URL,
         params={"key": API_KEY, "q": CITY}
     )
-    json_data = response.json()
-    message = (
-        f"{json_data['location']['country']}"
-        f"/{json_data['location']['name']} "
-        f"{json_data['location']['localtime']} "
-        f"Weather: {json_data['current']['temp_c']} Celsius, "
-        f"{json_data['current']['condition']['text']}"
 
-    )
-    print("Performing request to Weather API for city Paris...")
-    print(message)
+    if response.status_code == 200:
+        json_data = response.json()
+        message = (
+            f"{json_data['location']['country']}"
+            f"/{json_data['location']['name']} "
+            f"{json_data['location']['localtime']} "
+            f"Weather: {json_data['current']['temp_c']} Celsius, "
+            f"{json_data['current']['condition']['text']}"
+        )
+        print("Performing request to Weather API for city Paris...")
+        print(message)
+    else:
+        print("An api key was not provided or invalid!")
 
 
 if __name__ == "__main__":
